987c15bc4ed380d74a8fa8ce2aef67887c4630ad
StevanMcfc <stevasteva3@gmail.com>
2018-03-09 11:02:41 +0100

    Greskaaaaaaaaaaa

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -30,6 +30,8 @@ public class Biblioteka implements BibliotekaInterfejs {
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdavac) {
 				
+		
+		//Moj privatni kom
 		LinkedList<Knjiga> novaLista = new LinkedList<Knjiga>();
 		
 		if(naslov==null) throw new RuntimeException("Morate uneti naslov");
